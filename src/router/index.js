7877803c087695254e3fa3a7c34afb793e201022
Julian Namoc <IspyMILK@gmail.com>
2025-08-15 01:23:58 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import AppLayout from '@/layouts/AppLayout.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('@/views/guest/LoginView.vue'),
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: () => import('@/views/guest/registerView.vue'),
+        },
+        {
+            path: '/dashboard',
+            component: AppLayout,
+            children: [
+                {
+                    path: '/feed',
+                    name: 'feed',
+                    component: () => import('@/views/authenticated/FeedView.vue'),
+                },
+                {
+                    path: '/chats',
+                    name: 'chats',
+                    component: () => import('@/views/authenticated/chatsView.vue'),
+                },
+                {
+                    path: '/communities',
+                    name: 'communities',
+                    component: () => import('@/views/authenticated/communitiesView.vue'),
+                },
+                {
+                    path: '/assignments',
+                    name: 'assignments',
+                    component: () => import('@/views/authenticated/assignmentView.vue'),
+                },
+                {
+                    path: '/events',
+                    name: 'events',
+                    component: () => import('@/views/authenticated/eventsView.vue'),
+                },
+            ],
+        },
+    ],
+})
+
+export default router
